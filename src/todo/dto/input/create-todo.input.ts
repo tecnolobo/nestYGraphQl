89fb58dc57fd1cf38f4ca4b3ff43843ba1cd990a
julian gomez <gomezhtml@gmail.com>
2024-02-11 12:26:26 -0500

    Se crean validaciones para insertar un TODO

--- src/todo/dto/input/create-todo.input.ts
+++ src/todo/dto/input/create-todo.input.ts
@@ -1,8 +1,13 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { IsString,IsNotEmpty, MaxLength,MinLength } from 'class-validator';
 
 @InputType()
 export class CreateTodoInput{
 
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(20)
   @Field(
     ()=>String //de tipo string
     ,{description:"Que necesita hacer"}
